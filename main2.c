50c5f05da8838e0a3602c25d7e62058621fb8fce
David Guadalupe Vargas Lopez <david.vargas7480@alumnos.udg.mx>
2020-09-20 12:40:42 -0500

    se creo el arreglo y permite la entrada de 5 valores

--- main2.c
+++ main2.c
@@ -1,4 +1,24 @@
 #include<stdio.h>
+void capturar_enteros(){
+    int enteros[5];
+    float promedio;
+    int suma=0;
+    for (size_t i = 0; i < 5; i++)
+    {
+        scanf("%i",&enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("%i\n",enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++)
+    {
+        suma=suma+enteros[i];
+    }
+    printf ("suma: %i'\n",suma);
+    printf("promedio: %.2f\n",suma/5.0);
+    
+}
 
 int main()
 {
@@ -12,6 +32,19 @@ int main()
         printf("0.- salir \n");
         scanf("%c",&opc);
         fflush(stdin);
+         switch (opc)
+        {
+        case '1':
+            capturar_enteros();
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        
+        default:
+            break;
+        }
        
     } while (opc!='0');
     
